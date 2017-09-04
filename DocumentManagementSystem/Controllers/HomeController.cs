6474a2a8c65453389be861e258ff793d6d87aa7a
N <bigestyjoy@163.com>
2017-09-04 10:45:54 +0800

    调整主界面为自动跳转至文档一览

--- DocumentManagementSystem/Controllers/HomeController.cs
+++ DocumentManagementSystem/Controllers/HomeController.cs
@@ -10,7 +10,8 @@ namespace DocumentManagementSystem.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            //return View();
+            return RedirectToAction("Index", "Documents");
         }
 
         public ActionResult About()
