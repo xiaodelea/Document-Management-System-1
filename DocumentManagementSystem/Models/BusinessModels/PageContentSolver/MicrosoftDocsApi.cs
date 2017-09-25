eef0b9da21a70ce8da0d1129f6b958789ee3c65c
N <bigestyjoy@163.com>
2017-09-25 11:55:40 +0800

    建立API文档分析器
    未实现。

---
+++ DocumentManagementSystem/Models/BusinessModels/PageContentSolver/MicrosoftDocsApi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DocumentManagementSystem.Models.Domains.Entities;
+
+namespace DocumentManagementSystem.Models.BusinessModels.PageContentSolver
+{
+    public class MicrosoftDocsApi : Base
+    {
+        public MicrosoftDocsApi(string url) : base(url)
+        {
+        }
+
+        public override bool GetPage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Document GetReturn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ParsePage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
