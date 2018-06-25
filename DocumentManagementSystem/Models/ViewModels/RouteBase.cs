6fcd4e12c16120d570a312c0a40c9506d037f4a8
N <bigestyjoy@163.com>
2018-06-25 08:13:14 +0800

    Ver2.8.0 增加基架逻辑

---
+++ DocumentManagementSystem/Models/ViewModels/RouteBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.ViewModels
+{
+    public class RouteBase
+    {
+        public RouteBase(int page, int perPage, int count)
+        {
+            this.Page = page;
+            this.PerPage = perPage;
+            this.Count = count;
+
+            if (this.Page <= 0)
+                this.Page = 1;
+            if (this.PerPage <= 0)
+                this.PerPage = 20;
+        }
+
+
+
+
+
+        public int Page { get; set; }
+
+        public int PerPage { get; set; }
+
+        public int Count { get; set; }
+
+
+
+
+
+        public int MaxPage
+        {
+            get
+            {
+                return this.Count == 0 ? 0 : (int)Math.Ceiling((double)this.Count / this.PerPage);
+            }
+        }
+    }
+}
