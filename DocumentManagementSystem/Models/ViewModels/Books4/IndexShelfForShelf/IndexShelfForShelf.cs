ec0e61ace197b2f90b414e1c7ae07fb3271bd248
N <bigestyjoy@163.com>
2018-08-30 22:22:41 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/IndexShelfForShelf/IndexShelfForShelf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.IndexShelfForShelf
+{
+    public class IndexShelfForShelf
+    {
+        public IndexShelfForShelf(Guid parentDocumentId)
+        {
+            var shelf = new Worker.Books.Shelf.Index(parentDocumentId: parentDocumentId);
+
+            this.List = shelf.List.Select(c => new Item(c)).ToList();
+        }
+
+
+
+
+
+        public List<Item> List { get; set; }
+    }
+}
---
+++ DocumentManagementSystem/Models/ViewModels/Books4/IndexShelfForShelf/Item.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.IndexShelfForShelf
+{
+    public class Item
+    {
+        public Item(Worker.Books.Shelf.Details d)
+        {
+            if (!d._isExist)
+                return;
+
+            this.DocumentId = d.DocumentId;
+
+            this.Title = d.Title;
+            this.Priority = d.Priority;
+            this.UpdateTime = d.UpdateTime;
+
+            this._isExist = true;
+        }
+
+
+
+
+
+        public bool _isExist { get; set; }
+
+
+
+
+
+        public Guid DocumentId { get; set; }
+
+
+
+
+
+        [Display(Name = "书架名")]
+        public string Title { get; set; }
+
+        [Display(Name = "优先级")]
+        public int? Priority { get; set; }
+
+        [Display(Name = "更新时间")]
+        public DateTime UpdateTime { get; set; }
+    }
+}
