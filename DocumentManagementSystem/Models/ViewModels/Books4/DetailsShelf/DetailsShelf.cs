768ae639e5b7fb481fdbeadb6eaf5ae51ab21948
N <bigestyjoy@163.com>
2018-08-30 22:15:15 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/DetailsShelf/DetailsShelf.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.DetailsShelf
+{
+    public class DetailsShelf
+    {
+        public DetailsShelf(Guid id)
+        {
+            var book = new Worker.Books.Book.Details(id);
+            if (!book._isExist)
+                return;
+
+            this.DocumentId = book.DocumentId;
+
+            this.Title = book.Title;
+            this.Priority = book.Priority;
+            this.UpdateTime = book.UpdateTime;
+
+            this._isExist = true;
+        }
+
+
+
+
+
+        public bool _isExist { get; set; }
+
+
+
+
+
+        public Guid DocumentId { get; set; }
+
+
+
+
+
+        [Display(Name = "书名")]
+        public string Title { get; set; }
+
+        [Display(Name = "优先级")]
+        public int? Priority { get; set; }
+
+        [Display(Name = "更新时间")]
+        public DateTime UpdateTime { get; set; }
+    }
+}
