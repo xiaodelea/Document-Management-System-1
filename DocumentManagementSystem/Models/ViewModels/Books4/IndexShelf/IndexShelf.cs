65989753dd911d08f7565ef924f3395045b3decb
N <bigestyjoy@163.com>
2018-08-30 21:23:45 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/IndexShelf/IndexShelf.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.IndexShelf
+{
+    public class IndexShelf
+    {
+        public IndexShelf()
+        {
+            var shelf = new Worker.Books.Shelf.Index(isRoot: true);
+
+            this.List = new List<Item>();
+            var level = 0;
+
+            this.AddShelf(shelf, level);
+        }
+
+        private void AddShelf(Worker.Books.Shelf.Index i, int level)
+        {
+            foreach (var shelf in i.List)
+            {
+                this.List.Add(new Item(shelf, level));
+
+                var subShelf = new Worker.Books.Shelf.Index(parentDocumentId: shelf.DocumentId);
+
+                this.AddShelf(subShelf, level + 1);
+            }
+        }
+
+
+
+
+
+        public List<Item> List { get; set; }
+    }
+}
---
+++ DocumentManagementSystem/Models/ViewModels/Books4/IndexShelf/Item.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.IndexShelf
+{
+    public class Item
+    {
+        public Item(Worker.Books.Shelf.Details d, int level)
+        {
+            if (!d._isExist)
+                return;
+
+            this.DocumentId = d.DocumentId;
+            this.Level = level;
+
+            this.Title = d.Title;
+            this.Priority = d.Priority;
+            this.UpdateTime = d.UpdateTime;
+
+            this._isExist = true;
+        }
+
+
+
+
+
+        public bool _isExist { get; set; }
+
+
+
+
+
+        public Guid DocumentId { get; set; }
+
+        public int Level { get; set; }
+
+
+
+
+
+        [Display(Name = "书架名")]
+        public string Title { get; set; }
+
+        [Display(Name = "优先级")]
+        public int? Priority { get; set; }
+
+        [Display(Name = "更新时间")]
+        public DateTime UpdateTime { get; set; }
+    }
+}
