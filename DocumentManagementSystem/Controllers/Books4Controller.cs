484fbf71b68de90ca3369c098f569e8e0f126b02
N <bigestyjoy@163.com>
2018-08-30 19:11:00 +0800

    实现C

---
+++ DocumentManagementSystem/Controllers/Books4Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocumentManagementSystem.Controllers
+{
+    public class Books4Controller : Controller
+    {
+        public ActionResult IndexBook([Bind()]Models.ViewModels.Books4.IndexBook.Route route)
+        {
+            var i = new Models.ViewModels.Books4.IndexBook.IndexBook(route);
+
+            return View(i);
+        }
+    }
+}
