0318aa3ab0dcc58793a8bd1fb85ef5b46b45aed8
N <bigestyjoy@163.com>
2018-08-30 21:56:10 +0800

    实现W

---
+++ DocumentManagementSystem/Models/Worker/Books/Chapter/Index.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.Worker.Books.Chapter
+{
+    public class Index
+    {
+        public Index(int page = 1, int perpage = int.MaxValue, Guid? parentDocumentId = null)
+        {
+            var db = new Models.Domains.Entities.DMsDbContext();
+            var query = db.Documents.AsQueryable();
+
+            query = query.Where(c => c.IsBook && !c.IsMain && !c.IsAbstract);
+
+            if (parentDocumentId.HasValue)
+                query = query.Where(c => c.ParentDocumentId == parentDocumentId);
+
+            this.Count = query.Count();
+
+            var queryOrdered = query.OrderBy(c => c.Priority).ThenBy(c => c.DocumentId);
+            var queryCurrentPage = queryOrdered.Skip((page - 1) * perpage).Take(perpage);
+
+            this.List = queryCurrentPage.ToList().Select(c => new Details(c)).ToList();
+        }
+
+
+
+
+
+        public int Count { get; set; }
+
+        public List<Details> List { get; set; }
+    }
+}
