1debb135e846e1d55c9e9f866db7a8119f56cf04
N <bigestyjoy@163.com>
2018-08-30 17:50:44 +0800

    实现W

---
+++ DocumentManagementSystem/Models/Worker/Books/Book/Create.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.Worker.Books.Book
+{
+    public class Create
+    {
+        public Guid ParentDocumentId { get; set; }
+
+        public string Title { get; set; }
+
+        public int Priority { get; set; }
+
+
+
+
+
+        public Guid? DocumentId { get; set; }
+
+
+
+
+
+        public ValidateResult Save()
+        {
+            //验证参数。
+            if (string.IsNullOrWhiteSpace(this.Title))
+                return new ValidateResult(false, "Title", "标题不可为空！");
+            if (this.Priority < 0)
+                return new ValidateResult(false, "Priority", "序号不可小于0！");
+
+            //构建书籍。
+            var book = new Domains.Entities.Document
+            {
+                DocumentId = Guid.NewGuid(),
+                ParentDocumentId = this.ParentDocumentId,
+                Title = this.Title,
+                NodeName = this.Title,
+                Priority = this.Priority,
+                UpdateTimeForHTTPGet = DateTime.Now,
+                IsBook = true,
+                IsMain = true,
+                UpdateTime = DateTime.Now,
+            };
+
+            var db = new Domains.Entities.DMsDbContext();
+            lock (Atom.GetInstance())
+            {
+                //添加书籍。
+                db.Documents.Add(book);
+
+                db.SaveChanges();
+
+                //回传参数。
+                this.DocumentId = book.DocumentId;
+            }
+
+            return new ValidateResult(true);
+        }
+    }
+}
