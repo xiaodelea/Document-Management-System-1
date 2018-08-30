6f40897a5d5a153e2c75bcd2b8117a1cd9bebc24
N <bigestyjoy@163.com>
2018-08-30 17:54:24 +0800

    实现W

---
+++ DocumentManagementSystem/Models/Worker/Books/Book/Edit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.Worker.Books.Book
+{
+    public class Edit
+    {
+        public Guid DocumentId { get; set; }
+
+        public string Title { get; set; }
+
+        public int Priority { get; set; }
+
+        public bool IsChecked { get; set; }
+
+
+
+
+
+        public ValidateResult Save()
+        {
+            //验证参数。
+            if (string.IsNullOrWhiteSpace(this.Title))
+                return new ValidateResult(false, "Title", "标题不可为空！");
+            if (this.Priority < 0)
+                return new ValidateResult(false, "Priority", "序号不可小于0！");
+
+            var db = new Domains.Entities.DMsDbContext();
+            lock (Atom.GetInstance())
+            {
+                //验证书籍。
+                var book = db.Documents.Find(this.DocumentId);
+                if (!book.IsBook)
+                    return new ValidateResult(false, "DocumentId", "不是书籍！");
+
+                //修改书籍。
+                book.Title = this.Title;
+                book.Priority = this.Priority;
+                book.IsChecked = this.IsChecked;
+                book.UpdateTime = DateTime.Now;
+
+                db.SaveChanges();
+            }
+
+            return new ValidateResult(true);
+        }
+    }
+}
