1619111a2249d15494fd1433d39bf4cab39b7fec
N <bigestyjoy@163.com>
2018-08-31 11:05:12 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/IndexAddition/IndexAddition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.IndexAddition
+{
+    public class IndexAddition
+    {
+        public IndexAddition(Guid documentId)
+        {
+            var b = new Worker.Books.Addition.Index(documentId: documentId);
+
+            this.List = b.List.Select(c => new Item(c)).ToList();
+        }
+
+
+
+
+
+        public List<Item> List { get; set; }
+    }
+}
---
+++ DocumentManagementSystem/Models/ViewModels/Books4/IndexAddition/Item.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.IndexAddition
+{
+    public class Item
+    {
+        public Item(Worker.Books.Addition.Details d)
+        {
+            if (!d._isExist)
+                return;
+
+            this.AdditionId = d.AdditionId;
+
+            this.Name = d.Name;
+            this.Description = d.Description;
+
+            this._isExist = true;
+        }
+
+
+
+
+
+        public bool _isExist { get; set; }
+
+
+
+
+
+        public Guid AdditionId { get; set; }
+
+
+
+
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
