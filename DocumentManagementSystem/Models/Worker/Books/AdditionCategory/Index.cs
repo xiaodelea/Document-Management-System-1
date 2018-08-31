a6b5863d4ef7c9b9c079f8d1a199ca6f53711d17
N <bigestyjoy@163.com>
2018-08-31 11:32:15 +0800

    实现W

---
+++ DocumentManagementSystem/Models/Worker/Books/AdditionCategory/Index.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.Worker.Books.AdditionCategory
+{
+    public class Index
+    {
+        public Index(int page = 1, int perpage = int.MaxValue)
+        {
+            var db = new Models.Domains.Entities.DMsDbContext();
+            var query = db.AdditionCategories.AsQueryable();
+
+            var queryOrdered = query.OrderBy(c => c.Name);
+            var queryCurrentPage = queryOrdered.Skip((page - 1) * perpage).Take(perpage);
+
+            this.List = queryCurrentPage.ToList().Select(c => new Details(c)).ToList();
+        }
+
+
+
+
+
+        public int Count { get; set; }
+
+        public List<Details> List { get; set; }
+    }
+}
