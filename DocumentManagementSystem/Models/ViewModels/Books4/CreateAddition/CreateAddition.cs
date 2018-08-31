5616d02c58e21e6b63308cd8428d8fca806b63d9
N <bigestyjoy@163.com>
2018-08-31 11:34:36 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/CreateAddition/CreateAddition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.CreateAddition
+{
+    public class CreateAddition
+    {
+        public Guid DocumentId { get; set; }
+
+        [Required]
+        [Display(Name = "类别")]
+        public Guid? AdditionCategoryId { get; set; }
+
+        [Display(Name = "描述")]
+        public string Description { get; set; }
+
+
+
+
+
+        public Worker.ValidateResult Save()
+        {
+            var b = new Worker.Books.Addition.Create();
+
+            b.DocumentId = this.DocumentId;
+            b.AdditionCategoryId = this.AdditionCategoryId.Value;
+            b.Description = this.Description;
+
+            var result = b.Save();
+
+            return result;
+        }
+    }
+}
---
+++ DocumentManagementSystem/Models/ViewModels/Books4/CreateAddition/SelectionWorker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.CreateAddition
+{
+    public class SelectionWorker
+    {
+        public SelectionWorker(CreateAddition v)
+        {
+            var additionCategory_index = new Models.Worker.Books.AdditionCategory.Index();
+
+            this.AdditionCategories = new SelectList(additionCategory_index.List, "AdditionCategoryId", "Name", v.AdditionCategoryId);
+        }
+
+
+
+
+
+        public SelectList AdditionCategories { get; set; }
+    }
+}
