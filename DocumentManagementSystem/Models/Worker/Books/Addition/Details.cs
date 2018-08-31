b36e18737d4dc992ef995e2433bbe4615f03628d
N <bigestyjoy@163.com>
2018-08-31 10:57:43 +0800

    实现W

---
+++ DocumentManagementSystem/Models/Worker/Books/Addition/Details.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.Worker.Books.Addition
+{
+    public class Details
+    {
+        public Details(Guid id)
+        {
+            var db = new Domains.Entities.DMsDbContext();
+            var target = db.Additions.Find(id);
+            this.Initial(target);
+        }
+
+        public Details(object origin)
+        {
+            this.Initial(origin);
+        }
+
+        private void Initial(object origin)
+        {
+            if (origin == null)
+            {
+                this._isExist = false;
+                return;
+            }
+            else
+                this._isExist = true;
+
+            var target = (Domains.Entities.Addition)origin;
+
+            this.AdditionId = target.AdditionId;
+
+            this.Name = target.AdditionCategory.Name;
+            this.Description = target.Description;
+
+            this.UpdateTime = target.UpdateTime;
+            this.TimeStamp = BitConverter.ToInt64(target.TimeStamp, 0);
+        }
+
+
+
+
+
+        public bool _isExist { get; set; }
+
+
+
+
+
+        public Guid AdditionId { get; set; }
+
+
+
+
+
+        public string Name { get; set; }
+
+        public virtual string Description { get; set; }
+
+
+
+
+
+        public DateTime UpdateTime { get; set; }
+
+        public long TimeStamp { get; set; }
+    }
+}
