7d32ab2f4ec42f556551fcff466ed2a6550a6ff3
N <bigestyjoy@163.com>
2018-08-31 14:19:07 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/CreateBook/CreateBook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.CreateBook
+{
+    public class CreateBook
+    {
+        public CreateBook()
+        {
+
+        }
+
+        public CreateBook(Guid parentDocumentId)
+        {
+            this.ParentDocumentId = parentDocumentId;
+        }
+
+
+
+
+
+        public Guid ParentDocumentId { get; set; }
+
+        [Display(Name = "名称")]
+        public string Title { get; set; }
+
+        [Display(Name = "优先级")]
+        public int Priority { get; set; }
+
+
+
+
+
+        public Guid? DocumentId { get; set; }
+
+
+
+
+
+        public Worker.ValidateResult Save()
+        {
+            var b = new Worker.Books.Book.Create
+            {
+                ParentDocumentId = this.ParentDocumentId,
+                Title = this.Title,
+                Priority = this.Priority
+            };
+
+            var result = b.Save();
+
+            if (result.Result)
+                this.DocumentId = b.DocumentId;
+
+            return result;
+        }
+    }
+}
