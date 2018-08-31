33309a53b345e9c7a0daf057388d21d4a8f93e50
N <bigestyjoy@163.com>
2018-08-31 09:17:58 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/DirectoryBook/DirectoryBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.DirectoryBook
+{
+    public class DirectoryBook
+    {
+        public DirectoryBook(Guid parentDocumentId)
+        {
+            this.List = new List<Item>();
+
+            var book = new Worker.Books.Book.Details(parentDocumentId);
+            if (!book._isExist)
+                return;
+
+            var chapters = new Worker.Books.Chapter.Index(parentDocumentId: parentDocumentId);
+
+            this.AddChapter(chapters, 0);
+        }
+
+        private void AddChapter(Worker.Books.Chapter.Index i, int level)
+        {
+            foreach (var chapter in i.List)
+            {
+                this.List.Add(new Item(chapter, level));
+
+                var subChapter = new Worker.Books.Chapter.Index(parentDocumentId: chapter.DocumentId);
+
+                this.AddChapter(subChapter, level + 1);
+            }
+        }
+
+
+
+
+
+        public List<Item> List { get; set; }
+    }
+}
