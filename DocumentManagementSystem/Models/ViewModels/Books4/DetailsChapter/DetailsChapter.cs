cc08cffabcaad6da2975408e0d6db17c4b572f13
N <bigestyjoy@163.com>
2018-08-31 13:07:39 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/DetailsChapter/DetailsChapter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.DetailsChapter
+{
+    public class DetailsChapter
+    {
+        public DetailsChapter(Guid id)
+        {
+            var chapter = new Worker.Books.Chapter.Details(id);
+            if (!chapter._isExist)
+                return;
+
+            this.DocumentId = chapter.DocumentId;
+
+            this.Title = chapter.Title;
+            this.Priority = chapter.Priority;
+            this.IsChecked = chapter.IsChecked;
+            this.UpdateTime = chapter.UpdateTime;
+
+            this._isExist = true;
+        }
+
+
+
+
+
+        public bool _isExist { get; set; }
+
+
+
+
+
+        public Guid DocumentId { get; set; }
+
+
+
+
+
+        [Display(Name = "书名")]
+        public string Title { get; set; }
+
+        [Display(Name = "优先级")]
+        public int? Priority { get; set; }
+
+        [Display(Name = "查阅")]
+        public bool IsChecked { get; set; }
+
+        [Display(Name = "更新时间")]
+        public DateTime UpdateTime { get; set; }
+    }
+}
