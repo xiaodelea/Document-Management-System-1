ad529b4951424b9eb227335a86ba91188b35eeb2
N <bigestyjoy@163.com>
2018-08-31 14:04:25 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/DirectoryChapter/DirectoryChapter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.DirectoryChapter
+{
+    public class DirectoryChapter
+    {
+        public DirectoryChapter(Guid documentId)
+        {
+            this.List = new List<Item>();
+
+            var chapter = new Worker.Books.Chapter.Details(documentId);
+            if (!chapter._isExist)
+                return;
+
+            this.AddSilbingChapter(chapter.ParentDocumentId, documentId);
+
+            this.AddParent(chapter.ParentDocumentId, -1);
+
+            this.Solve();
+        }
+
+        private void AddSilbingChapter(Guid parentDocumentId, Guid documentId)
+        {
+            var chapters = new Models.Worker.Books.Chapter.Index(parentDocumentId: parentDocumentId);
+
+            foreach (var chapter in chapters.List)
+            {
+                this.List.Insert(0, new Item(chapter, 0, chapter.DocumentId == documentId));
+            }
+        }
+
+        private void AddParent(Guid parentDocumentId, int level)
+        {
+            var chapter = new Worker.Books.Chapter.Details(parentDocumentId);
+
+            if (chapter._isExist)
+            {
+                this.List.Add(new Item(chapter, level, false));
+                this.AddParent(chapter.ParentDocumentId, level - 1);
+            }
+            else
+            {
+                var book = new Worker.Books.Book.Details(parentDocumentId);
+                this.List.Add(new Item(book, level));
+            }
+        }
+
+        private void Solve()
+        {
+            this.List.Reverse();
+
+            var max = -this.List.First().Level;
+
+            this.List.ForEach(c => c.Level += max);
+        }
+
+
+
+
+
+        public List<Item> List { get; set; }
+    }
+}
---
+++ DocumentManagementSystem/Models/ViewModels/Books4/DirectoryChapter/Item.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.DirectoryChapter
+{
+    public class Item
+    {
+        public Item(Worker.Books.Chapter.Details d, int level, bool isCurrentChapter)
+        {
+            if (!d._isExist)
+                return;
+
+            this.DocumentId = d.DocumentId;
+            this.Level = level;
+            this.IsCurrentChapter = isCurrentChapter;
+
+            this.Title = d.Title;
+            this.Priority = d.Priority;
+            this.IsChecked = d.IsChecked;
+
+            this._isExist = true;
+        }
+
+        public Item(Worker.Books.Book.Details d, int level)
+        {
+            if (!d._isExist)
+                return;
+
+            this.DocumentId = d.DocumentId;
+            this.Level = level;
+            this.IsBook = true;
+
+            this.Title = d.Title;
+            this.Priority = d.Priority;
+            this.IsChecked = d.IsChecked;
+
+            this._isExist = true;
+        }
+
+
+
+
+
+        public bool _isExist { get; set; }
+
+
+
+
+
+        public Guid DocumentId { get; set; }
+
+        public int Level { get; set; }
+
+        public bool IsCurrentChapter { get; set; }
+
+        public bool IsBook { get; set; }
+
+
+
+
+
+        [Display(Name = "子章节")]
+        public string Title { get; set; }
+
+        [Display(Name = "优先级")]
+        public int? Priority { get; set; }
+
+        [Display(Name = "查阅")]
+        public bool IsChecked { get; set; }
+    }
+}
