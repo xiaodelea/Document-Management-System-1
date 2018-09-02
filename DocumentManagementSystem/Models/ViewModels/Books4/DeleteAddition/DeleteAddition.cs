bdc6bdc0462ca2e55cf30234b4d3985e9cdb49e8
N <bigestyjoy@163.com>
2018-09-02 19:16:39 +0800

    实现MV

---
+++ DocumentManagementSystem/Models/ViewModels/Books4/DeleteAddition/DeleteAddition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.DeleteAddition
+{
+    public class DeleteAddition
+    {
+        public Guid AdditionId { get; set; }
+
+        public long TimeStamp { get; set; }
+
+
+
+
+
+        public Worker.ValidateResult Save()
+        {
+            var b = new Worker.Books.Addition.Delete
+            {
+                AdditionId = this.AdditionId,
+                TimeStamp = this.TimeStamp,
+            };
+
+            var result = b.Save();
+
+            return result;
+        }
+    }
+}
---
+++ DocumentManagementSystem/Models/ViewModels/Books4/EditAddition/EditAddition.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentManagementSystem.Models.ViewModels.Books4.EditAddition
+{
+    public class EditAddition
+    {
+        public EditAddition()
+        {
+
+        }
+
+        public EditAddition(Guid additionId)
+        {
+            var b = new Worker.Books.Addition.Details(additionId);
+
+            this._isExist = b._isExist;
+            if (!b._isExist)
+                return;
+
+            this.AdditionId = b.AdditionId;
+            this.TimeStamp = b.TimeStamp;
+
+            this.Description = b.Description;
+        }
+
+
+
+
+
+        public bool _isExist { get; set; }
+
+
+
+
+
+        public Guid AdditionId { get; set; }
+
+        public long TimeStamp { get; set; }
+
+
+
+
+
+        [Display(Name = "说明")]
+        public string Description { get; set; }
+
+
+
+
+
+        public Worker.ValidateResult Save()
+        {
+            var b = new Worker.Books.Addition.Edit
+            {
+                AdditionId = this.AdditionId,
+                TimeStamp = this.TimeStamp,
+                Description = this.Description,
+            };
+
+            var result = b.Save();
+
+            return result;
+        }
+    }
+}
